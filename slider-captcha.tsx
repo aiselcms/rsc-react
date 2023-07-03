a76d05b0805cd812d1389b18e1167d29c7ed9cd6
user <aiselcms.aj@gmail.com>
2023-07-03 16:33:07 +0300

    props type fix 4

--- slider-captcha.tsx
+++ slider-captcha.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import * as PropTypes from 'prop-types';
 import Anchor from './anchor';
 import Theme from './theme';
 
