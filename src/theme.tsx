36eb199158495967d585d09404156166062128e3
user <aiselcms.aj@gmail.com>
2023-07-04 15:23:13 +0300

    js -> ts 0.02

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import lightTheme from "./style/light";
 import darkTheme from "./style/dark";
-import { AppTheme, ThemeProps } from "@/interfaces/interfaces";
+import { AppTheme, ThemeProps } from "./interfaces/interfaces";
 
 const Theme: FC<ThemeProps> = ({ theme }) => (
   <style suppressHydrationWarning>
