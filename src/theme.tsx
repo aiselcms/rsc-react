816d331181f145e6898dbe3c37f3998dc41722d5
user <aiselcms.aj@gmail.com>
2023-07-04 16:17:50 +0300

    js -> ts 0.06

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react";
 import lightTheme from "./style/light";
 import darkTheme from "./style/dark";
-import { AppTheme, ThemeProps } from "./interfaces/interfaces";
+import { CaptchaTheme, ThemeProps } from "./interfaces/interfaces";
 
 const Theme: FC<ThemeProps> = ({ theme }) => (
   <style suppressHydrationWarning>
-    {theme === AppTheme.dark ? darkTheme : lightTheme}
+    {theme === CaptchaTheme.dark ? darkTheme : lightTheme}
   </style>
 );
 
