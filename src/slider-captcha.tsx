10770d3a7f850826ca94804b2f2b9feab7f20f8c
user <aiselcms.aj@gmail.com>
2023-07-06 12:39:41 +0300

    add styles

--- src/slider-captcha.tsx
+++ src/slider-captcha.tsx
@@ -9,7 +9,7 @@ import {
   VerifyCaptchaCallback,
 } from "./interfaces/interfaces";
 import ThemeProvider from "./providers/ThemeProvider";
-import "./styles/style.css";
+import "./styles/styles.scss";
 
 const fetchCaptcha =
   (create: CreateCaptchaCallback) => async (): Promise<CaptchaResult> => {
